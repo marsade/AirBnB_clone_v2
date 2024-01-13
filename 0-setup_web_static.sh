e693a868166c49aeeb855785d23598f59df1c00e
marsade <adenijifara@gmail.com>
2024-01-13 18:10:14 +0200

    Add sudo command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,26 +2,26 @@
 # a scripts that sets up the web server for deployment of web_static
 
 # installs nginx
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
 # creates folder /data
-mkdir -p /data/
+sudo mkdir -p /data/
 
 # creates folder /data/web_static
-mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/
 
 # creates folder /data/web_static/releases
-mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/
 
 # creates folder /data/web_static/shared
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 
 # creates folder /data/web_static/releases/test/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test/
 
 # creates a fake HTML file with content
-touch /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
@@ -31,14 +31,14 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 # creates a symbolink link /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # gives ownership of /data/ folder to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Nginx configuration
-sed -i '/listen 80 default/a\ \tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default/a\ \tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # restarts nginx
-service nginx restart
+sudo service nginx restart
 
