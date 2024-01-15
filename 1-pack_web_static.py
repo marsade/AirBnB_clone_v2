d356fd19bb1253d5d4a03cbac9e3601bd7784b9c
marsade <adenijifara@gmail.com>
2024-01-15 16:24:55 +0200

    Modifications to file_name

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Generate a tgz archive containing the content from web_static
-from fabric.api import local
+from fabric.api import local, runs_once
 from datetime import datetime
 import os
 
-
+@runs_once
 def do_pack():
     """Archive function"""
     try:
@@ -13,9 +13,14 @@ def do_pack():
         now = datetime.now()
         file_name = "webstatic{}{}{}{}{}{}.tgz".format(
             now.year, now.month, now.day,
-            now.hour, now.minute
+            now.hour, now.minute, now.second
         )
+        print("Packing web_static to versions/web_static_{}".format(file_name))
         local("tar -cvzf versions/{} web_static".format(file_name))
+        dir = "versions/{}".format(file_name)
+        size = os.stat(dir).st_size
+        print("web_static packed: {} -> {} Bytes".format(dir, size))
         return "versions/{}".format(file_name)
     except Exception as e:
+        print("Error: {}".format(e))
         return None
