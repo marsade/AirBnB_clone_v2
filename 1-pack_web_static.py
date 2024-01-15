0eba1b7860e744c23b7bfd58a5b9515a44ae0ad5
marsade <adenijifara@gmail.com>
2024-01-15 16:14:32 +0200

    Task complete

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,17 +2,19 @@
 # Generate a tgz archive containing the content from web_static
 from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
     """Archive function"""
     try:
+        if not os.path.isdir("versions"):
+            os.mkdir("versions")
         now = datetime.now()
         file_name = "webstatic{}{}{}{}{}{}.tgz".format(
             now.year, now.month, now.day,
             now.hour, now.minute
         )
-        local("mkdir -p versions")
         local("tar -cvzf versions/{} web_static".format(file_name))
         return "versions/{}".format(file_name)
     except Exception as e:
