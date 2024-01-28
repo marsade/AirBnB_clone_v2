3cac51d1af1713c808a9a7eb38abf02072e89438
marsade <adenijifara@gmail.com>
2024-01-28 11:28:22 +0200

    Fix comments

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,15 +5,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     """Returns hello world on the / route
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB!'    
 
 
 if __name__ == '__main__':
-    """Run the Flask app on 0.0.0.0, port 5000
-    """
+    """Run the Flask app on 0.0.0.0, port 5000"""
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
