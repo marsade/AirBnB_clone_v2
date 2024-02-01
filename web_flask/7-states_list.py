387fce63c3e3db0a648b336d9433ef15b46b923d
marsade <adenijifara@gmail.com>
2024-02-01 11:46:53 +0200

    Task 7 complete

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Starts a Flask appplication"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close(error):
+    storage.close()
+
+
+@app.route('/states_list')
+def list_states():
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    """Run the appplication"""
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
